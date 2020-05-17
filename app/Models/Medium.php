c5768274c25c5a663e38ac5657dae648ae8a6119
Ramses Moreno <ramses@cuatromedios.com>
2020-05-17 10:30:38 -0500

    Changed the place where media presets are defined, from config to the medium model

--- app/Models/Medium.php
+++ app/Models/Medium.php
@@ -10,46 +10,19 @@ class Medium extends MediumModel
      * Define the presets the images can use for server side optimization
      */
     const PRESETS = [
-        'thumb' => [
-            'quality' => 80,
-            'width' => 320,
-            'height' =>  320,
-            'background' => 'crop',
-            'alignment' => 'center',
-            'scale' => 'cover',
-            'format' => 'jpg',
-            'effects' => []
-        ],
-        'preview' => [
-            'quality' => 95,
-            'width' => 1200,
-            'height' =>  1200,
-            'background' => 'crop',
-            'alignment' => 'center',
-            'scale' => 'cover',
-            'format' => 'jpg',
-            'effects' => []
-        ],
-        'logo' => [
-            'quality' => 100,
-            'width' => 128,
-            'height' =>  128,
-            'background' => 'crop',
-            'alignment' => 'center',
-            'scale' => 'contain',
-            'format' => 'png',
-            'effects' => []
-        ]
+        'thumb' =>   ['quality' => 80,  'width' => 320,  'height' =>  320,  'background' => 'crop', 'alignment' => 'center', 'scale' => 'cover',  'format' => 'jpg',  'effects' => []],
+        'preview' => ['quality' => 95,  'width' => 1200, 'height' =>  1200, 'background' => 'crop', 'alignment' => 'center', 'scale' => 'cover',  'format' => 'jpg',  'effects' => []],
+        'logo' =>    ['quality' => 100, 'width' => 128,  'height' =>  128,  'background' => 'crop', 'alignment' => 'center', 'scale' => 'contain', 'format' => 'png', 'effects' => []]
     ];
     /**
-     * Create an accesor for every preset
+     * Create accessors for every preset
      */
     public function getThumbAttribute()   { return $this->getUrl('thumb'); }
     public function getPreviewAttribute() { return $this->getUrl('preview'); }
     public function getLogoAttribute()    { return $this->getUrl('logo'); }
 
     /**
-     * Optionally append the accesors so the queries result includes the url
+     * Optionally append the accessors so the queries result includes the url
      */
     protected $appends = ['thumb', 'preview', 'logo'];
 }
