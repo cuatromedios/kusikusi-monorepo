edf6cebbde13cb41224579efed4224a7fb96408a
Ramses Moreno <ramses@cuatromedios.com>
2020-05-16 19:29:01 -0500

    Fixing the support to have different formats, for example .amp .rss

--- resources/views/html/partials/socialshare.blade.php
+++ resources/views/html/partials/socialshare.blade.php
@@ -1,3 +1,4 @@
+@if(isset($entity))
 @php
 // Get the social image from the meiudm tagged appended to the entity, if none, then the medium appended
 // to the website entity, if not, a default image
@@ -13,3 +14,4 @@ $social_image = config('cms.app_url', '') . Illuminate\Support\Arr::get($entity,
 <meta property="og:site_name" content="{{ isset($website) ? $website->title : '' }}">
 <meta property="og:locale" content="{{ $lang ?? 'en' }}">
 <meta property="og:type" content="website">
+@endif
