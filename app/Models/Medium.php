b04556ca23a7a344bf75a07d88107f4e9f864b7c
Ramses <ramses@cuatromedios.com>
2020-05-26 14:22:39 -0500

    Version 4.1.1 Media management improvements

--- app/Models/Medium.php
+++ app/Models/Medium.php
@@ -12,7 +12,7 @@ class Medium extends MediumModel
      * Define the presets the images can use for server side optimization
      */
     const PRESETS = [
-        'thumb' =>   ['quality' => 80,  'width' => 320,  'height' =>  320,  'background' => 'crop', 'alignment' => 'center', 'scale' => 'cover',  'format' => 'jpg',  'effects' => []],
+        'thumb' =>   ['quality' => 100, 'width' => 320,  'height' =>  320,  'background' => 'crop', 'alignment' => 'center', 'scale' => 'cover',  'format' => 'png',  'effects' => []],
         'preview' => ['quality' => 95,  'width' => 1200, 'height' =>  1200, 'background' => 'crop', 'alignment' => 'center', 'scale' => 'cover',  'format' => 'jpg',  'effects' => []],
         'logo' =>    ['quality' => 100, 'width' => 128,  'height' =>  128,  'background' => 'crop', 'alignment' => 'center', 'scale' => 'contain', 'format' => 'png', 'effects' => []]
     ];
