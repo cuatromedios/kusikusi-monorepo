54cd57ee516ae381c24425d325bebe43077b732e
Ramses <ramses@cuatromedios.com>
2021-03-10 00:09:20 -0600

    The samples of scopes are shown as the result array, not one by one

--- kusikusi-dev/resources/views/entities/show.blade.php
+++ kusikusi-dev/resources/views/entities/show.blade.php
@@ -18,51 +18,37 @@
 
     @if (isset($childrenOfEntity) && count($childrenOfEntity) > 0)
         <h4>Children of Entity</h4>
-        @foreach ($childrenOfEntity as $child)    
-            {!! pretty_json($child) !!}
-        @endforeach
+        {!! pretty_json($childrenOfEntity) !!}
     @endif
 
     @if (isset($parentOfEntity) && count($parentOfEntity) > 0)
-        <h4>Parent of Entity</h4>
-        @foreach ($parentOfEntity as $parent)    
-            {!! pretty_json($parent) !!}
-        @endforeach
+        <h4>Parent of Entity</h4>    
+        {!! pretty_json($parentOfEntity) !!}
     @endif
 
     @if (isset($ancestorsOfEntity) && count($ancestorsOfEntity) > 0)
-        <h4>Ancestors of Entity</h4>
-        @foreach ($ancestorsOfEntity as $ancestor)    
-            {!! pretty_json($ancestor) !!}
-        @endforeach
+        <h4>Ancestors of Entity</h4>)    
+        {!! pretty_json($ancestorsOfEntity) !!}
     @endif
 
     @if (isset($descendantsOfEntity) && count($descendantsOfEntity) > 0)
-        <h4>Descendants of Entity</h4>
-        @foreach ($descendantsOfEntity as $descendant)    
-            {!! pretty_json($descendant) !!}
-        @endforeach
+        <h4>Descendants of Entity</h4>    
+        {!! pretty_json($descendantsOfEntity) !!}
     @endif
     
     @if (isset($siblingsOfEntity) && count($siblingsOfEntity) > 0)
         <h4>Siblings of Entity</h4>
-        @foreach ($siblingsOfEntity as $sibling)    
-            {!! pretty_json($sibling) !!}
-        @endforeach
+        {!! pretty_json($siblingsOfEntity) !!}
     @endif
     
     @if (isset($relatedByEntity) && count($relatedByEntity) > 0)
-        <h4>Related by Entity</h4>
-        @foreach ($relatedByEntity as $related)    
-            {!! pretty_json($related) !!}
-        @endforeach
+        <h4>Related by Entity</h4>   
+        {!! pretty_json($relatedByEntity) !!}
     @endif
     
     @if (isset($relatingEntity) && count($relatingEntity) > 0)
-        <h4>Relating Entity</h4>
-        @foreach ($relatingEntity as $relating)    
-            {!! pretty_json($relating) !!}
-        @endforeach
+        <h4>Relating Entity</h4>  
+        {!! pretty_json($relatingEntity) !!}
     @endif
 
     <a href="{{ route('entities.create') }}">Create</a>
