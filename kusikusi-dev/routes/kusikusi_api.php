e553f758809dee1be9fdf821ef9dfe23d0fa195e
Ramses <ramses@cuatromedios.com>
2021-03-25 00:20:05 -0600

    Entity Relations endpoints

--- kusikusi-dev/routes/kusikusi_api.php
+++ kusikusi-dev/routes/kusikusi_api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Kusikusi\Http\Controllers\EntityController;
+use Kusikusi\Http\Controllers\EntityRelationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,6 @@ use Kusikusi\Http\Controllers\EntityController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-$id_rule='[A-Za-z0-9_-]{1,32}';
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,4 +25,8 @@ Route::post('/entities', [EntityController::class, 'store']);
 Route::patch('/entities/{entity_id}', [EntityController::class, 'update']);
 Route::delete('/entities/{entity_id}', [EntityController::class, 'destroy']);
 Route::patch('/entities/{entity_id}/restore', [EntityController::class, 'restore']);
-Route::post('/entities/{entity_id}/relation', [EntityController::class, 'restore']);
+Route::get('/entities/{entity_id}/relations', [EntityRelationController::class, 'index']);
+Route::post('/entities/{entity_id}/relations/{called_entity_id}/{kind}', [EntityRelationController::class, 'store']);
+Route::delete('/entities/{entity_id}/relations/{called_entity_id}/{kind}', [EntityRelationController::class, 'destroy']);
+Route::delete('/entities/{entity_id}/relations/{relation_id}', [EntityRelationController::class, 'destroy']);
+Route::patch('/entities/{entity_id}/relations/reorder', [EntityRelationController::class, 'reorder']);
---
+++ packages/api/src/Http/Controllers/EntityRelationController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Kusikusi\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Validation\Rule;
+use Illuminate\Routing\Controller as BaseController;
+use Kusikusi\Models\Entity;
+use Kusikusi\Models\EntityRelation;
+
+class EntityRelationController extends BaseController
+{
+
+    const ID_RULE = 'string|min:1|max:32|regex:/^[A-Za-z0-9_-]{1,32}$/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * List relations of an entity, this is an alias of /entities?related-by={entity_id}.
+     *
+     * @group Entity
+     * @authenticated
+     * @urlParam entity_id The id of the entity to create relations to.
+     */
+    public function index(Request $request, $entity_id)
+    {
+        $request->merge([
+            'related-by' => $entity_id,
+            'order-by' => "relation_position"
+        ]);
+        $entityController = new EntityController();
+        return $entityController->index($request);
+    }
+    
+    /**
+     * Stores a new entity relation, or replace it if already exist.
+     *
+     * @group Entity
+     * @authenticated
+     * @urlParam entity_id The id of the entity to create relations to.
+     * @urlParam called_entity_id string Required. The the id of the entity to relate
+     * @urlParam kind string Required. The type of relation. Example: medium, category, author...
+     * @bodyParam position number Optional. A non signed integer to set the position of the relation in reference to other relations. Example 0, 1, 2
+     * @bodyParam depth number Optional. A non signed integer to set the depth of the relation in reference to the entity. Example 0, 1, 2
+     * @bodyParam tags array Optional. An array of strings to tag the relation. Example icon, main
+     * @responseFile responses/entities.create.json
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request, $entity_id, $called_entity_id, $kind)
+    {
+        $request->validate([
+            'position' => 'integer',
+            'depth' => 'integer',
+            'tags.*' => 'string'
+        ]);
+       $relation = EntityRelation::create(array_merge(
+           $request->only('position', 'depth', 'tags'),
+           [
+            "caller_entity_id" => $entity_id,
+            "called_entity_id" => $called_entity_id,
+            "kind" => $kind
+           ]
+        ));
+        return($relation);
+    }
+
+
+    /**
+     * Deletes a relation.
+     *
+     * @group Entity
+     * @authenticated
+     * @urlParam entity_id The id of the entity to delete a relations to.
+     * @urlParam called_entity_id string Required. The the id of the entity related
+     * @urlParam kind string Required. The type of relation. Example: medium, category, author...
+     */
+    public function destroy(Request $request, $entity_id, $called_entity_or_relation_id, $kind = null)
+    {
+        if ($kind === null) {
+            $deletion = EntityRelation::where('relation_id', $called_entity_or_relation_id)->delete();
+        } else {
+            $deletion = EntityRelation::where('caller_entity_id', $entity_id)
+            ->where('called_entity_id', $called_entity_or_relation_id)
+            ->where('kind', $kind)
+            ->delete();
+        }
+        
+        return ([ "deletes" => $deletion]);
+    }
+
+    /**
+     * Reorders an array of relations
+     *
+     * Receive an array of relation ids, and sets the individual position to its index in the array.
+     *
+     * @group Entity
+     * @authenticated
+     * @bodyParam relation_ids array required An array of relation ids to reorder. Example [30, 31, 32, 33]
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function reorder(Request $request, $entity_id)
+    {
+        $request->validate([
+            'relation_ids' => 'required',
+            'relation_ids.*' => (new EntityRelation)->getKeyType()
+        ]);
+        $relations = EntityRelation::select('relation_id', 'position')->where('caller_entity_id', $entity_id)->find($request['relation_ids']);
+        for ($r = 0; $r < $relations->count(); $r++) {
+            
+            $relations[$r]->position = array_search($relations[$r]->relation_id, $request['relation_ids']) ?? 0;
+            $relations[$r]->save();
+        }
+        return(["data" => $relations]);
+    }
+}
