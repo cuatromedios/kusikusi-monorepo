681fb1ec08b9cf8ec472686cbdfbb4e8328c4277
Ramses <ramses@cuatromedios.com>
2021-10-10 02:25:09 -0500

    Always show the menu for all users

--- admin/src/store/modules/ui.js
+++ admin/src/store/modules/ui.js
@@ -78,11 +78,7 @@ const getters = {
   menu: (state, getters, rootState) => {
     let menu = _.clone(_.get(state, `config.menu.${rootState.session.user.profile ? rootState.session.user.profile : 'admin'}`))
     if (!menu) {
-      if (rootState.session.user.profile === 'admin') {
         menu = [state.menuItems.content, state.menuItems.menus, state.menuItems.website, state.menuItems.media]
-      } else {
-        menu = [state.menuItems.content]
-      }
     }
     menu.push(state.menuItems.logout)
     return menu
